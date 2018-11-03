60cac89ca1f4f71d78aca6aa7bca024dae30be5f
Dimitri Merejkowsky <d.merej@gmail.com>
2018-11-03 20:56:29 +0100

    Switch to dmenv

--- setup.py
+++ setup.py
@@ -10,26 +10,37 @@ def get_long_description():
         return fp.read()
 
 
-setup(name="python-cli-ui",
-      version="0.7.1",
-      description="Build Nice User Interfaces In The Terminal",
-      long_description=get_long_description(),
-      url="https://github.com/TankerHQ/python-cli-ui",
-      author="Dimitri Merejkowsky",
-      packages=find_packages(),
-      include_package_data=True,
-      install_requires=[
-          "colorama",
-          "tabulate",
-          "unidecode",
-      ],
-      classifiers=[
-          "Programming Language :: Python :: 3.5",
-          "Programming Language :: Python :: 3.6",
-      ],
-      entry_points={
-          "console_scripts": [
-              "tsrc = tsrc.cli.main:main",
-          ]
-      }
-      )
+setup(
+    name="python-cli-ui",
+    version="0.7.1",
+    description="Build Nice User Interfaces In The Terminal",
+    long_description=get_long_description(),
+    url="https://github.com/TankerHQ/python-cli-ui",
+    author="Dimitri Merejkowsky",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "colorama",
+        "tabulate",
+        "unidecode"
+    ],
+    extras_require={
+        "dev": [
+            "sphinx",
+            "ghp-import",
+            "pytest",
+            "pyflakes"
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    entry_points={
+        "console_scripts":[
+            "tsrc = tsrc.cli.main:main"
+        ]
+    },
+)
