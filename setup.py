3fe642a141cb12ee2404941dd69d736935051eea
Dimitri Merejkowsky <dimitri.merejkowsky@tanker.io>
2018-11-06 14:04:24 +0100

    Switch to dmenv

--- setup.py
+++ setup.py
@@ -23,9 +23,19 @@ setup(name="python-cli-ui",
           "tabulate",
           "unidecode",
       ],
+      extras_require={
+          "dev": [
+              "sphinx",
+              "ghp-import",
+              "pytest",
+              "pyflakes",
+          ],
+      },
       classifiers=[
+          "Programming Language :: Python :: 3.4",
           "Programming Language :: Python :: 3.5",
           "Programming Language :: Python :: 3.6",
+          "Programming Language :: Python :: 3.7",
       ],
       entry_points={
           "console_scripts": [
