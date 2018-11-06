5a3674781095f287859a17f534b4e629f19cd086
Dimitri Merejkowsky <dimitri.merejkowsky@tanker.io>
2018-11-06 14:16:01 +0100

    Bump to 0.7.3

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def get_long_description():
 
 
 setup(name="python-cli-ui",
-      version="0.7.2",
+      version="0.7.3",
       description="Build Nice User Interfaces In The Terminal",
       long_description=get_long_description(),
       url="https://github.com/TankerHQ/python-cli-ui",
