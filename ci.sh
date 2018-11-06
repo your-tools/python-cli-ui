5343e80d8ae491ca46017830dc5fa8007263ac8d
Dimitri Merejkowsky <dimitri.merejkowsky@tanker.io>
2018-11-06 14:04:24 +0100

    Switch to dmenv

--- ci.sh
+++ ci.sh
@@ -1,6 +1,6 @@
 set -x
 set -e
 
-poetry run pyflakes ui/__init__.py ui/tests/test_ui.py
-poetry run pytest
-poetry run sphinx-build -W docs docs/_build/html
+pyflakes ui/__init__.py ui/tests/test_ui.py
+pytest
+sphinx-build -W docs docs/_build/html
