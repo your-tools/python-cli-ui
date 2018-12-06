6ba7f6774596dcc81d9693ed86dcbb2c678afa39
Dimitri Merejkowsky <dimitri.merejkowsky@tanker.io>
2018-12-06 16:14:36 +0100

    Use setup.cfg instead of setup.py
    Much more readable, don't you think?

--- setup.py
+++ setup.py
@@ -1,40 +1,7 @@
 import sys
-from setuptools import setup, find_packages
+import setuptools
 
 if sys.version_info.major < 3:
     sys.exit("Error: Please upgrade to Python3")
 
-
-def get_long_description():
-    with open("README.rst") as fp:
-        return fp.read()
-
-
-setup(name="python-cli-ui",
-      version="0.7.4",
-      description="Build Nice User Interfaces In The Terminal",
-      long_description=get_long_description(),
-      url="https://github.com/TankerHQ/python-cli-ui",
-      author="Dimitri Merejkowsky",
-      packages=find_packages(),
-      include_package_data=True,
-      install_requires=[
-          "colorama",
-          "tabulate",
-          "unidecode",
-      ],
-      extras_require={
-          "dev": [
-              "sphinx",
-              "ghp-import",
-              "pytest",
-              "pyflakes",
-          ],
-      },
-      classifiers=[
-          "Programming Language :: Python :: 3.4",
-          "Programming Language :: Python :: 3.5",
-          "Programming Language :: Python :: 3.6",
-          "Programming Language :: Python :: 3.7",
-      ],
-      )
+setuptools.setup()
