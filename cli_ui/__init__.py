ba54050c2aea5c6bc18384d40bfe90fa8648e0cc
Dimitri Merejkowsky <dimitri.merejkowsky@tanker.io>
2019-04-17 15:58:05 +0200

    Bump to mypy 0.700

--- cli_ui/__init__.py
+++ cli_ui/__init__.py
@@ -397,7 +397,10 @@ def tabs(num: int) -> str:
 
 
 def info_table(
-    data: Any, *, headers: Optional[Sequence[str]] = None, fileobj: Any = None
+    data: Any,
+    *,
+    headers: Union[str, Dict[str, str], Sequence[str]] = (),
+    fileobj: Any = None
 ) -> None:
     if not fileobj:
         fileobj = sys.stdout
