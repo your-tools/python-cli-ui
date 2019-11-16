4561e2402c51f6f7d71c9540dbc945e04fd07200
Dimitri Merejkowsky <d.merej@gmail.com>
2019-11-16 17:14:39 +0100

    Move mypy options into a config file

--- ci.sh
+++ ci.sh
@@ -2,7 +2,7 @@ set -x
 set -e
 
 which black && black --check .
-MYPYPATH=stubs mypy --ignore-missing-imports --strict cli_ui
+mypy cli_ui
 pyflakes cli_ui/__init__.py cli_ui/tests/test_cli_ui.py
 pytest --cov . --cov-report term
 sphinx-build -W docs docs/_build/html
