cb84374b4c5521153eb347aef246a240c84d2f04
Dimitri Merejkowsky <dimitri.merejkowsky@tanker.io>
2020-02-04 17:18:20 +0100

    Fix typos in publish script

--- publish.sh
+++ publish.sh
@@ -4,12 +4,12 @@ set -x
 set -e
 
 # Build and upload doc
-poetry run phinx-build -W docs docs/_build/html/
+poetry run sphinx-build -W docs docs/_build/html/
 poetry run ghp-import \
         --push \
         --force \
         --no-jekyll \
-        docs/_build/htmtl \
+        docs/_build/html/ \
 
 # Build and upload source and wheel on pypi.org
 rm -fr dist/
