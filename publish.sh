4090dd77113de51e373b8b4673cdc30035648af5
Dimitri Merejkowsky <dimitri.merejkowsky@tanker.io>
2020-02-04 17:09:37 +0100

    Fix publication script

--- publish.sh
+++ publish.sh
@@ -3,6 +3,15 @@
 set -x
 set -e
 
+# Build and upload doc
+poetry run phinx-build -W docs docs/_build/html/
+poetry run ghp-import \
+        --push \
+        --force \
+        --no-jekyll \
+        docs/_build/htmtl \
+
+# Build and upload source and wheel on pypi.org
 rm -fr dist/
-python setup.py sdist bdist_wheel
-twine upload dist/*
+poetry build
+poetry publish
