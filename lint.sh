3674f32229d2c9e0b85bf6b9d8ba62573d3d4726
Dimitri Merejkowsky <dimitri@dmerej.info>
2021-05-08 18:13:35 +0200

    Remove lint.sh script

--- lint.sh
+++
@@ -1,8 +0,0 @@
-set -x
-set -e
-
-poetry run black --check .
-poetry run isort --check .
-poetry run mypy cli_ui
-poetry run flake8 cli_ui/__init__.py cli_ui/tests/test_cli_ui.py
-poetry run sphinx-build -W docs docs/_build/html
